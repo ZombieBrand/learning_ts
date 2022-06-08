333b002168927b92ab1b1b2bdb4620f8b9588c6e
ZombieBrand <zombiebrandg@gmail.com>
2022-06-08 16:57:39 +0800

    easy-push&unshift and easy-includes complete

---
+++ src/easy-includes/template.ts
@@ -0,0 +1,14 @@
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
+
+export type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
---
+++ src/easy-push&unshift/template.ts
@@ -0,0 +1,2 @@
+export type Push<T extends unknown[], U> = [...T, U]
+export type Unshift<T extends unknown[], U> = [U, ...T]
